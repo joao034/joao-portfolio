8ba779c1cc77526f205d297be37c5800c28bf9fc
Joao <joaojacomedev@gmail.com>
2024-09-05 15:17:41 -0500

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center text-blue-500 mt-8">
+  ¡Hola, Tailwind con React y Vite!
+</h1>
+
+    </div>
+  )
+}
+
+
